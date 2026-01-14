92e2f2367549df74f577905b45a4fb1c0efa94a0
544-byte <544@cifpceuta.es>
2026-01-14 19:11:35 +0100

    Borrado del Main (test funcionamiento git)

--- src/Misc/Main.java
+++ src/Misc/Main.java
@@ -9,8 +9,6 @@ import Combat.Combate;
 
 public class Main {
     public static void main(String[] args) {
-        Personaje c1 = new Personaje("Cristian",18,50,25,25,10,10);
-        Personaje c2 = new Personaje("Alejandro",18,25,50,25,10,10);
-        Combate.combatir(c1, c2);
+
     }
 }
