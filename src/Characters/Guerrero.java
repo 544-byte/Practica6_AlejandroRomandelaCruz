834253ab0a6ea187032c173d2bb93373124fb4f6
544-byte <544@cifpceuta.es>
2026-01-16 21:16:24 +0100

    Primera parte completa y finalizando la segunda (accion especial)

---
+++ src/Characters/Guerrero.java
@@ -0,0 +1,129 @@
+package Characters;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guerrero extends Personaje{
+    private boolean furia;
+
+    // region Constructores
+    public Guerrero() {
+        super();
+        setFuria(false);
+    }
+
+    public Guerrero(String nombre, int nivel, double pv, double atq, double arm, double res, double vel, boolean furia) {
+        super(nombre, nivel, pv, atq, arm, res, vel);
+        setFuria(furia);
+    }
+
+    public Guerrero(String nombre, boolean furia) {
+        super(nombre);
+        setFuria(furia);
+    }
+
+    public Guerrero(String nombre, int nivel, boolean furia) {
+        super(nombre, nivel);
+        setFuria(furia);
+    }
+
+    public Guerrero(Personaje p, boolean furia) {
+        super(p);
+        setFuria(furia);
+    }
+    // endregion
+
+    public void subirNivel() {
+        Random r = new Random();
+        if (r.nextInt(100) < 75) setPv(getPv() +1);
+        if (getFuria()) {
+            setAtq(getAtq()/2);
+            if (r.nextInt(100) < 80) setAtq(getAtq() +2);
+            setAtq(getAtq()*2);
+        } else if (r.nextInt(100) < 80) setAtq(getAtq() +2);
+        if (getFuria()) {
+            setAtq(getAtq()*2);
+            if (r.nextInt(100) < 75) setArm(getArm() +1);
+            setAtq(getAtq()/2);
+        } else if (r.nextInt(100) < 75) setArm(getArm() +1);
+        if (r.nextInt(100) < 20) setRes(getRes() +1);
+        this.setNivel(getNivel() +1);
+    }
+
+    public void modificarFuria(){
+        if (furia) setFuria(false);
+        else setFuria(true);
+    }
+    // region Setters & Getters
+    public boolean getFuria() {
+        return furia;
+    }
+
+    public void setFuria(boolean furia) {
+        if (this.furia == false && furia == true){
+            setAtq(getAtq() * 2);
+            setArm(getArm() / 2);
+        } else if (this.furia == true && furia == false){
+            setAtq(getAtq() / 2);
+            setArm(getArm() * 2);
+        }
+        this.furia = furia;
+
+    }
+
+    public void accionEspecial(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Furia: \n" +
+                "Furia aumenta tu poder de ataque el doble a cambio de reducir a la mitad tu armadura \n" +
+                "¿Estás seguro de tu acción? s/n");
+        char opt = scan.nextLine().toLowerCase().charAt(0);
+        switch (opt){
+            case 's' -> {
+                modificarFuria();
+                System.out.println("¡Furia activada!");
+                // po aqui voyyy
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+    }
+
+    public static String GetAccionEspecial(){
+        return "Furia";
+    }
+    // endregion
+
+
+    // region Overrides
+    public Guerrero clone() {
+        return new Guerrero(super.clone(), this.getFuria());
+    }
+
+    public boolean equals(Guerrero g) {
+        return getNombre().equals(g.getNombre()) &&
+                getPv() == g.getPv() &&
+                getAtq() == g.getAtq() &&
+                getArm() == g.getArm() &&
+                getVel() == g.getVel() &&
+                getRes() == g.getRes() &&
+                getNivel() == g.getNivel() &&
+                getFuria() == g.getFuria();
+    }
+
+    public String toString() {
+        return ("Clase: Guerrero\n" +
+                super.toString() + "\n" +
+                "¿Furioso? " + getFuria()
+        );
+    }
+    // endregion
+}
--- src/Misc/Main.java
+++ src/Misc/Main.java
@@ -10,5 +10,8 @@ import Combat.Combate;
 public class Main {
     public static void main(String[] args) {
         System.out.println("prueba");
+        Personaje p1 = new Personaje("P1",1,3,1,0,0,1);
+        Personaje p2 = new Personaje("P2",1,1,1,0,0,5);
+        Combate.combatir(p1,p2);
     }
 }
